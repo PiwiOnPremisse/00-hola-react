0044cf47f6f87968848cea022970fd2c350c3f16
PiwiOnPremisse <116581079+MatiasZenith@users.noreply.github.com>
2023-06-23 22:57:01 -0400

    Modificacion 1

--- src/App.jsx
+++ src/App.jsx
@@ -23,11 +23,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit aaaa <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more sdsd
+        Click on the Vite and React logos to learn more 
       </p>
     </>
   )
