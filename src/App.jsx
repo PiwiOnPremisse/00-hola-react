e8de0221845a92b6aada8805d5f0bbf807906a94
PiwiOnPremisse <116581079+MatiasZenith@users.noreply.github.com>
2023-06-24 03:40:08 -0400

    Ultimo

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+export function Ap(){
+    return(
+        <article>
+            <header>
+                <img src="https://yt3.googleusercontent.com/vRF8BHREiJ3Y16AbMxEi_oEuoQlnNNqGpgULuZ6zrWSAi24HcxX3Vko42RN8ToctH-G0qlWd=s176-c-k-c0x00ffffff-no-rj" alt="" />
+                <div>
+                    <strong>Miguel Angel Vilalon</strong>
+                    <span>@minudev</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    
+    )
+}
+
+export function Mapa(){
+    return(
+        <div>
+            <h1>Twitter Card</h1>
+            <p>Cola cola de 2 litros</p>
+        </div>
+    
+    )
+}
+
+
+
+
